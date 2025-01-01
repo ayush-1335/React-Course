931c5934f5f9000f5614488a65d4b3ecf96718a1
ayush-1335 <ayushkevadiya9@gmail.com>
2025-01-01 23:18:13 +0530

    learn hooks

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,13 +1,34 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
+  let [counter, setCounter] = useState(10) 
+  // let counter = 10;
+
+  const addValue = () => {
+    // counter = counter + 1
+    if(counter <20){
+      setCounter(counter + 1)
+    }
+    
+  }
+
+  const removeValue = () => {
+    // counter = counter - 1
+    if(counter > 0){
+      setCounter(counter - 1)
+    }
+  }
+
   return (
     <>
       <h1> Counter </h1>
+      <h2> Counter Value : {counter}</h2>
+      <div><button onClick={addValue}> Add value </button></div>
+      <div><button onClick={removeValue}> Decrease value </button></div>
     </>
   )
 }
